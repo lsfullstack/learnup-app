0fd32bf8a68d75e2fd965d598d2ed5f0eb74c80f
Lucas <lsengenheirocivil@gmail.com>
2023-06-03 11:25:10 -0300

    feat: create target-link component

---
+++ src/components/TargetLink/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import { ITargetLinkProps } from "./interface"
+
+export const TargetLink = ({href, children, styleType}: ITargetLinkProps) => {
+  const defaultStyle: string = `w-fit font-enphasis hover:underline`;
+
+  return (
+    <Link 
+      className={`
+        ${styleType === "blue" && `text-brand-3 ${defaultStyle}`}
+        ${styleType === "grey" && `text-grey-4 ${defaultStyle}`}
+      `}
+      href={href}
+    >{children}</Link>
+  );
+}
